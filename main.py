ce41d457e11159b7907c43a8657d3e45a541b28a
iKoogar <koo_z@bellsouth.net>
2020-11-24 01:42:50 -0500

    trying uvicorn

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import Services.mongo_setup as mongo_setup
 import Services.data_service as svc
-from fastapi import FastAPI, Query
 
 import Services.fast_api as fa
 import uvicorn
 import json
-
-app = FastAPI()
 
 
 def read_secrets():
@@ -19,8 +16,8 @@ def main():
     secrets = read_secrets()
     mongo_setup.global_init(secrets)
 
-
-    uvicorn.run(app, host="0.0.0.0", port=8123)
+    app = fa.app
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
     #svc.createUser("poog", "poog@gamr.com", "yeet")
     svc.find_user_by_email("poog@gamr.com")
@@ -28,3 +25,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
