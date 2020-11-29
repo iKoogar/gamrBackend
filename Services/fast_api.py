50a7ecea0574f1660cc8061e02f56bbf221fbaab
Zachary Koo <koo_z@bellsouth.net>
2020-11-29 03:38:17 -0500

    added find user by email endpoint

--- Services/fast_api.py
+++ Services/fast_api.py
@@ -4,14 +4,22 @@ import Services.data_service as svc
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.post("/newUserTest/{name}")
 async def newUser(nam: str):
     svc.addUser(nam, "noemail", "nopwd")
 
+
 @app.get("/allUsers")
 async def getAllUsers():
     return svc.getAllUsers()
+
+
+@app.get("/userByEmail/{usr}")
+async def userByEmail(usr):
+    return svc.findUserByEmail(usr)
